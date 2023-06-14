5ea85f2360fac5aed4e6504cb7bbee15f9e93cd7
Alka <rashinkaralka@gmail.com>
2023-06-14 16:30:01 +0530

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import ResetPasswordLink from './Component/ResetPasswordLink';
-import ResetPasswordform from './Component/ResetPasswordform';
-import SignIn from './Component/SignIn';
-import SignUp from './Component/SignUp';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ResetPasswordLink from "./Component/ResetPasswordLink";
+import ResetPasswordform from "./Component/ResetPasswordform";
+import SignIn from "./Component/SignIn";
+import SignUp from "./Component/SignUp";
 
 function App() {
   return (
-      <Routes>
-      <Route path="/" element={<SignIn />}  />
-      <Route path="/resetpasslink" element={<ResetPasswordLink/>} />
-      <Route path="/resetpassform" element={<ResetPasswordform/>} />
-      <Route path="/signup" element={<SignUp/>} />
-      </Routes>
-  
+    <Routes>
+      <Route exact path="/" element={<SignIn />} />
+      <Route exact path="/resetpasslink" element={<ResetPasswordLink />} />
+      <Route exact path="/resetpassform" element={<ResetPasswordform />} />
+      <Route exact path="/signup" element={<SignUp />} />
+    </Routes>
   );
 }
 
